97c7e7b3405f149f57ab01f3580f1cd6531b768d
Alfiya <114033078+abdimutalipova-alfiya@users.noreply.github.com>
2025-01-23 15:14:22 +0100

    .

--- main_dutch.py
+++ main_dutch.py
@@ -1,7 +1,7 @@
 import sounddevice as sd
 import os
 from stt.stt_whisper import SpeechToTextAgent
-from llm_dutch_geitje import LLMAgent
+from llm_dutch import LLMAgent
 from tts.tts import TTSAgent
 from manager_dutch_backup import ManagerAgent
 
@@ -13,11 +13,11 @@ def main():
 
     manager = ManagerAgent(llm_agent, stt_agent, tts_agent, "https://dev1.sebastiaandaniels.com/", 1)
 
-    welcome_message = """Let op, dit gesprek wordt opgenomen en jouw feedback zal binnen ons bedrijf worden gebruikt. Deel alsjeblieft geen persoonlijke informatie met de bank! 
-    Hallo! Ik ben jouw vriendelijke stadfeedbackbank. Ik ben hier om jouw gedachten over onze stad te horen en je ideeën te verzamelen om het nog beter te maken. Wil je jouw ervaringen met mij delen? 
-        """
+    # welcome_message = """Let op, dit gesprek wordt opgenomen en jouw feedback zal binnen ons bedrijf worden gebruikt. Deel alsjeblieft geen persoonlijke informatie met de bank! 
+    # Hallo! Ik ben jouw vriendelijke stadfeedbackbank. Ik ben hier om jouw gedachten over onze stad te horen en je ideeën te verzamelen om het nog beter te maken. Wil je jouw ervaringen met mij delen? 
+    #     """
 
-    manager.tts_agent.text_to_speech(welcome_message)
+    # manager.tts_agent.text_to_speech(welcome_message)
 
     print("Starting conversation loop...")
 

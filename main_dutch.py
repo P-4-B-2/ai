ef9608277d1a52c4353616e55e2751b2f9edc21e
Alfiya <114033078+abdimutalipova-alfiya@users.noreply.github.com>
2025-02-13 13:14:55 +0100

    .

--- main_dutch.py
+++ main_dutch.py
@@ -1,16 +1,16 @@
 from stt.stt_whisper import SpeechToTextAgent
 from llm_dutch import LLMAgent
 from tts.tts import TTSAgent
-from manager_dutch import ManagerAgent
+from manager_dutch_backup import ManagerAgent
 
 
 def main():
-    
+
     llm_agent = LLMAgent()
     stt_agent = SpeechToTextAgent(model_name="base")
     tts_agent = TTSAgent()
 
-    manager = ManagerAgent(llm_agent, stt_agent, tts_agent, "https:/frankdepratendebank.azurewebsites.net/", 1)
+    manager = ManagerAgent(llm_agent, stt_agent, tts_agent, "https://frankdepratendebank.azurewebsites.net/api/", 1)
 
     # welcome_message = """Let op, dit gesprek wordt opgenomen en jouw feedback zal binnen ons bedrijf worden gebruikt. Deel alsjeblieft geen persoonlijke informatie met de bank! 
     # Hallo! Ik ben jouw vriendelijke stadfeedbackbank. Ik ben hier om jouw gedachten over onze stad te horen en je ideeën te verzamelen om het nog beter te maken. Wil je jouw ervaringen met mij delen? 
